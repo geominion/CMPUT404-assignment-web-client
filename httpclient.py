3436b62d890ce163fba267e6aca523e499fed125
geominion <joshua@ualberta.ca>
2014-02-03 21:29:21 -0700

    Update httpclient.py

--- httpclient.py
+++ httpclient.py
@@ -19,7 +19,6 @@
 
 import sys
 import socket
-import re
 # you may use urllib to encode data appropriately
 import urllib
 
@@ -33,20 +32,52 @@ class HTTPRequest(object):
 
 class HTTPClient(object):
     #def get_host_port(self,url):
-
+            
     def connect(self, host, port):
-        # use sockets!
-        return None
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        return sock
 
     def get_code(self, data):
-        return None
+        broken = data.split()
+        print broken[1]
+        return int(broken[1])
 
     def get_headers(self,data):
-        return None
+        first = data.split("//")
+        if len(first) > 1:
+            second = first[1].split("/")
+            setpath = first[1]
+        else:
+            second = first[0].split("/")
+            setpath = first[0]
+        host = second[0]
+        if len(second) > 1:
+            path = setpath[len(host):]
+        else:
+            path = "/"
+        parts = [path, host]
+        return parts
 
     def get_body(self, data):
-        return None
+        broken = data.split("\r")
+        body = ""
+        check = False
+        for line in broken:
+            if line == "\n":
+                check = True
+            if check:
+                body = body + line
+        return body[2:]
 
+    def get_port(self, data):
+        parts = data.split(":")
+        if len(parts) < 1:
+            return parts
+        else:
+            parts.insert(1, 80)
+        return parts
+    
     # read everything from the socket
     def recvall(self, sock):
         buffer = bytearray()
@@ -60,13 +91,36 @@ class HTTPClient(object):
         return str(buffer)
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        parts = self.get_headers(url)
+        path = parts[0]
+        host = self.get_port(parts[1])
+        if args != None:
+            encargs = urllib.urlencode(args)
+            path = path + "?" + encargs
+        request = "GET " + path + " HTTP/1.1\r\nHost:" + parts[1] + "\r\nAccept:*/*\r\n\r\n"
+        sock = self.connect(host[0], host[1])
+        sock.sendall(request)
+        response = self.recvall(sock)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        print body
         return HTTPRequest(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        parts = self.get_headers(url)
+        host = self.get_port(parts[1])
+        request = "POST " + parts[0] + " HTTP/1.1\r\nHost:" + parts[1] + "\r\nContent-type:application/x-www-form-urlencoded\r\ncontent-length:"
+        if args != None:
+            encargs = urllib.urlencode(args)
+            request = request + str(len(encargs)) + "\r\n\r\n" + encargs
+        else:
+            request = request + "0" + "\r\n\r\n"
+        sock = self.connect(host[0], host[1])
+        sock.sendall(request)
+        response = self.recvall(sock)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        print body
         return HTTPRequest(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -77,11 +131,11 @@ class HTTPClient(object):
     
 if __name__ == "__main__":
     client = HTTPClient()
-    command = "GET"
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print client.command( sys.argv[1], sys.argv[2] )
+        print client.command(sys.argv[2], sys.argv[1])
     else:
-        print client.command( command, sys.argv[1] )    
+        print client.command(sys.argv[1])
+
